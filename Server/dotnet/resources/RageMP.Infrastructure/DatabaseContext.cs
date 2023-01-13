73d6084f95d7e6896afe051f4672697f9b279b01
Roman Petrychenko <Coveragt@gmail.com>
2023-01-13 16:14:57 +0200

    Initial

---
+++ Server/dotnet/resources/RageMP.Infrastructure/DatabaseContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace Infrastructure
+{
+    public class DatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            const string connectionString = "User ID =postgres;Host=localhost;Port=5432;Database=;Password=123;";
+
+            optionsBuilder.UseNpgsql(connectionString, sqlOptions =>
+            {
+                sqlOptions.EnableRetryOnFailure(
+                    10,
+                    TimeSpan.FromSeconds(30),
+                    null);
+            });
+
+            optionsBuilder.LogTo(Console.WriteLine, LogLevel.Information);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+    }
+}
